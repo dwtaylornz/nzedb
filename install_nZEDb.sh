33ba625c8cb0ce390340f59c8bddfa2e300e35f3
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-02 20:50:20 +1300

    Update install_nZEDb.sh

--- install_nZEDb.sh
+++ install_nZEDb.sh
@@ -1,5 +1,5 @@
 # Install nZEDb.sh
-# cd /var/www
-# git clone https://github.com/nZEDb/nZEDb.git
+cd /var/www
+git clone https://github.com/nZEDb/nZEDb.git
 
 # composer install --no-dev --keep-vcs --prefer-source
