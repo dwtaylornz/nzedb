41d8aaf8370a53c0304259c4f67f5cbb691eff10
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-04 16:24:36 +1300

    Create install_Step11_nZEDb.sh

---
+++ install_Step11_nZEDb.sh
@@ -0,0 +1,20 @@
+newgrp www-data
+cd /var/www/
+mkdir -p /var/www
+sudo chown www-data:www-data /var/www
+sudo chmod 775 /var/www
+
+# Install Composer 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+git clone https://github.com/nZEDb/nZEDb.git
+cd /var/www/nZEDb
+composer install --prefer-source
+
+sudo chmod -R 755 /var/www/nZEDb/app/libraries
+sudo chmod -R 755 /var/www/nZEDb/libraries
+sudo chmod -R 777 /var/www/nZEDb/resources
+sudo chmod -R 777 /var/www/nZEDb/www
