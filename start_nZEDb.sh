c03dc8528f67794f28446efc0a8b14ad4faa0ef4
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2017-10-04 15:27:45 +1300

    Update start_nZEDb.sh

--- start_nZEDb.sh
+++ start_nZEDb.sh
@@ -1,11 +1,13 @@
 # Start nZDEDb
+
+echo ""
+echo "--- Start nZEDb ---"   
 echo ""
 echo "  _____                 ____  "
 echo " |   __|_ _ ___ ___ ___|    \ "
 echo " |__   | | | . | -_|  _|  |  |"
 echo " |_____|___|  _|___|_| |____/ "
 echo "           |_|                "
-echo ""
-echo "--- Start nZEDb ---"                     
+echo ""            
 
 tail -F /install_nZEDb.sh
